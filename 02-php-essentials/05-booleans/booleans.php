5f1dfe069ef1e72ba768cc79976c5accad170376
Aneta Kasprzak <anet.kasprzak@gmail.com>
2025-01-24 14:27:48 +0000

    !, &&, ||, xor operators

--- 02-php-essentials/05-booleans/booleans.php
+++ 02-php-essentials/05-booleans/booleans.php
@@ -11,33 +11,53 @@
 <body>
     <pre><?php
 
-            var_dump(true);
-            var_dump(false);
+            // var_dump(true);
+            // var_dump(false);
 
-            $value = true;
-            var_dump($value);
+            // $value = true;
+            // var_dump($value);
+
+            // echo "----\n";
+
+            // $meaning = 42;
+            // var_dump($meaning > 13);
+            // var_dump(42 > 13);
+            // var_dump((40 + 2) >= 13);
+
+            // echo "----\n";
+
+            // $name = 'Aneta';
+            // var_dump($name === 'Anetaa');
+            // var_dump($name !== 'Anetaa');
+
+
+            // $age = 32;
+            // var_dump($age === 32); //true
+            // var_dump($age === '32'); //false
+            // var_dump($age == '32'); //true
+            // var_dump($age != '32'); //false
+
+
+            $number = 5;
+            var_dump(!($number === 5)); //false
 
             echo "----\n";
 
-            $meaning = 42;
-            var_dump($meaning > 13);
-            var_dump(42 > 13);
-            var_dump((40 + 2) >= 13);
+            //and operator
+            var_dump(true && true);
 
-            echo "----\n";
+            $views = 50000;
+            if ($views >= 10000 && $views <= 100000) {
+                echo 'This is a medium performing video';
+            };
 
-            $name = 'Aneta';
-            var_dump($name === 'Anetaa');
-            var_dump($name !== 'Anetaa');
+            echo "\n----\n";
 
+            //or operator
+            var_dump(false || false); // output=>false, at least one side has to be true
 
-            $age = 32;
-            var_dump($age === 32); //true
-            var_dump($age === '32'); //false
-            var_dump($age == '32'); //true
-            var_dump($age != '32'); //false
-
-
+            //xor operator - not used often
+            var_dump(true xor true); //either or statement, expects only one true, here output will be false
 
             ?></pre>
 </body>
