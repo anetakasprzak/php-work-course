679654f81b1455dabe21f7ad33ba74e2ac331303
Aneta Kasprzak <anet.kasprzak@gmail.com>
2025-01-28 09:44:52 +0000

    submit a form through GET

--- 05-params-and-forms/03-form/form.php
+++ 05-params-and-forms/03-form/form.php
@@ -9,9 +9,19 @@
 </head>
 
 <body>
-    <pre></pre>
+    <pre><?php
+            // Retrieve user input from the url
+            var_dump($_GET);
+            ?></pre>
 
+    <?php if (!empty($_GET['book'])): ?>
+        <h1><?php echo $_GET['book']; ?></h1>
+    <?php endif; ?>
 
+    <form action="form.php" method="GET">
+        <input type="text" name="book" value="<?php if (!empty($_GET['book'])) echo $_GET['book']; ?>" />
+        <input type="submit" value="Submit!" />
+    </form>
 
 
 
