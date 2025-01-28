9bcb0d1a821d657c362159d851d00b7f775723b0
Aneta Kasprzak <anet.kasprzak@gmail.com>
2025-01-28 09:23:10 +0000

    $_GET

---
+++ 05-params-and-forms/00-short-if/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$name = 'Jan';
+
+// if ($name === 'Jan') echo "The name is: Jan";
+// else echo "The name is not Jan";
+
+echo ($name === 'Jan' ? 'The name is: Jan' : 'The name is not Jan');
